a51b711dc3b76d12b890445278a64516f1ec44a4
Jan Halama <halama@jablotron.cz>
2020-08-09 20:05:30 +0200

    test: add coverage of untested files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testRegex: '^(.*\\.)(test|spec)\\.(ts|js)$',
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/build/'],
 };
